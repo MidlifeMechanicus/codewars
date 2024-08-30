3be07134323d1fbcbd92192b9e7ded48ebbae8e0
ElectronDragon <electricdragondevelopment@gmail.com>
2024-08-30 09:02:12 +0100

    Build a square

---
+++ 7-kyu/7-kyu-build-a-square/main.rb
@@ -0,0 +1,15 @@
+def generate_shape(n)
+  string = "+" * n
+  array = []
+  n.times { array << string }
+  array.join("\n")
+end
+
+# This one threw me for a bit, because #times has an implicit return of (integer), NOT the block.
+# As such, I thought my code was not working, even though it was.
+# Refactored to:
+
+def generate_shape(n)
+  string = "+" * n
+  string + ("\n" + string) * (n - 1)
+end
